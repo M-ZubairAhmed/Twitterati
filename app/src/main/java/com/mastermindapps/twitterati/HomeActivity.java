be6d9fc0b0a1737263324a4b419a1d0899c93fa4
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-04-11 23:39:15 +0530

    alert dia for seach

--- app/src/main/java/com/mastermindapps/twitterati/HomeActivity.java
+++ app/src/main/java/com/mastermindapps/twitterati/HomeActivity.java
@@ -1,19 +1,22 @@
 package com.mastermindapps.twitterati;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,6 +30,7 @@ public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     NavigationView navigationView;
+     String q = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +48,7 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String query = getSearchQuery();
             }
         });
 
@@ -139,9 +142,29 @@ public class HomeActivity extends AppCompatActivity
     void logoutTwitter() {
         Twitter.getSessionManager().clearActiveSession();
         Twitter.logOut();
-        Intent gobackSignin = new Intent(HomeActivity.this,SigninActivity.class);
+        Intent gobackSignin = new Intent(HomeActivity.this, SigninActivity.class);
         startActivity(gobackSignin);
         finish();
-        Toast.makeText(getApplicationContext(),"You are logged out",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "You are logged out", Toast.LENGTH_LONG).show();
+    }
+
+    String getSearchQuery() {
+        LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+        View alertdialogCustomView = inflater.inflate(R.layout.search_alertdia_layout, null);
+        final EditText queryEditT = (EditText) alertdialogCustomView.findViewById(R.id.search_query_editt_xml);
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(alertdialogCustomView)
+                .setTitle("Enter your search query")
+                .setPositiveButton("Search", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        q = queryEditT.getText().toString().trim().toLowerCase();
+                        Toast.makeText(HomeActivity.this,q,Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .create();
+        alertDialog.show();
+        return q;
     }
 }
+
