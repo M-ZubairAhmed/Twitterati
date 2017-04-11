ff7d262d534b7b74d0a640260115d9d813acb38d
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-04-11 17:47:19 +0530

    sign in with twitter adde

--- app/src/main/java/com/mastermindapps/twitterati/SigninActivity.java
+++ app/src/main/java/com/mastermindapps/twitterati/SigninActivity.java
@@ -31,18 +31,13 @@ public class SigninActivity extends AppCompatActivity {
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-                // The TwitterSession is also available through:
-                // Twitter.getInstance().core.getSessionManager().getActiveSession()
                 TwitterSession session = result.data;
-                // TODO: Remove toast and use the TwitterSession's userID
-                // with your app's user model
-                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void failure(TwitterException exception) {
                 Log.d("TwitterKit", "Login with Twitter failure", exception);
+                Toast.makeText(SigninActivity.this, R.string.signin_problem, Toast.LENGTH_LONG).show();
             }
         });
 
@@ -51,8 +46,6 @@ public class SigninActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // Make sure that the loginButton hears the result from any
-        // Activity that it triggered.
         loginButton.onActivityResult(requestCode, resultCode, data);
     }
 
