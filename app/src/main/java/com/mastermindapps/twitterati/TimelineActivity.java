7455a38c710f2b090b00765d49ec4498ada316e0
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-04-13 00:17:27 +0530

    bug fix of action bar overlay
    previously the timeline was underlapping action bar, but both are
    distinct

--- app/src/main/java/com/mastermindapps/twitterati/TimelineActivity.java
+++ app/src/main/java/com/mastermindapps/twitterati/TimelineActivity.java
@@ -14,7 +14,7 @@ public class TimelineActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timeline);
+        setContentView(R.layout.app_bar_timeline);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
